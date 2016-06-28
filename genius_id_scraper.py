0488106d5474c88c63332325ef42a6c6bef8f2c4
Nima Boscarino <nboscari@sfu.ca>
2016-06-27 23:09:26 -0700

    completed genius id scraper, now running. will soon push updated json file

--- genius_id_scraper.py
+++ genius_id_scraper.py
@@ -1,15 +1,27 @@
+import pymongo
 import requests
 import sys
 from lxml import html
 import re
 
-GENIUS_URL = "http://www.genius.com/artists/"
-artist = sys.argv[1]
+client = pymongo.MongoClient()
+db = client.databased
 
-artist_genius_response = requests.get(GENIUS_URL + artist)
-tree = html.fromstring(artist_genius_response.content)
-genius_id_raw = tree.xpath('//form[@class="edit_artist"]/@action')[0]
-m = re.search('/artists/(.+)', genius_id_raw)
-if m:
-  genius_id = m.group(1)
-print genius_id
+GENIUS_URL = "http://www.genius.com/artists/"
+
+artists = db.artists.find()
+
+for artist in artists:
+  print artist['name']
+  artist_name = artist['name'].replace('.', '')
+  artist_genius_response = requests.get(GENIUS_URL + artist_name)
+  tree = html.fromstring(artist_genius_response.content)
+  genius_id_raw = tree.xpath('//form[@class="edit_artist"]/@action')[0]
+  m = re.search('/artists/(.+)', genius_id_raw)
+  if m:
+    genius_id = m.group(1)
+  result = db.artists.update_one(
+    {"id": artist['id']},
+      {"$set": {"genius_id": int(genius_id)}}
+    )
+  print genius_id
