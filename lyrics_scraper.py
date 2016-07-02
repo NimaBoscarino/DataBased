fa5f1cf5f81287b377c6cbafaa4c9e5b31972911
Nima Boscarino <nboscari@sfu.ca>
2016-07-01 21:00:52 -0700

    fixed duplication when inserting lyrics

--- lyrics_scraper.py
+++ lyrics_scraper.py
@@ -14,14 +14,15 @@ total = end - start
 count = 0
 
 for song in songs:
-  lyrics = {}
-  lyrics['title'] = song['title']
-  lyrics['genius_id'] = song['genius_id']
-  count = count + 1
-  print "\n" + str(count) + "/" + str(total) + " " + song['title']
-  lyrics_url = song['lyrics_url']
-  r = requests.get(lyrics_url)
-  tree = html.fromstring(r.content)
-  raw_lyrics = tree.xpath('//lyrics//text()')
-  lyrics['text'] = ''.join(raw_lyrics)
-  db.lyrics.insert(lyrics)
+  if db.lyrics.count({"genius_id":song['genius_id']:
+    lyrics = {}
+    lyrics['title'] = song['title']
+    lyrics['genius_id'] = song['genius_id']
+    count = count + 1
+    print "\n" + str(count) + "/" + str(total) + " " + str(start + count - 1) + " " + song['title']
+    lyrics_url = song['lyrics_url']
+    r = requests.get(lyrics_url)
+    tree = html.fromstring(r.content)
+    raw_lyrics = tree.xpath('//lyrics//text()')
+    lyrics['text'] = ''.join(raw_lyrics)
+    db.lyrics.insert(lyrics)
